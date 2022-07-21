408419eb420108496ed1da992fa010cf2cb44e2a
dsh-dsh <dan.shipilov@gmail.com>
2022-07-21 20:02:16 +0300

    add hard deleting docs

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -35,8 +35,8 @@
             </div>
         </div>
       </AccordionTab>
-      <AccordionTab header="Header II">
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+      <AccordionTab header="Безвозвратное удаление документов">
+        <Button label="Удалить помеченные на удаление документы" @click="deleteDocs" class="p-button-sm p-button-text" />
       </AccordionTab>
       <AccordionTab header="Header III">
           <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
@@ -162,6 +162,9 @@ export default {
     },
     setProperty(user, type, value) {
       this.$store.dispatch('setProperty', [user, type, value]);
+    },
+    deleteDocs() {
+      this.$store.dispatch('deleteDocs');
     }
   },
 }
